b3ad18526438dfdd27ae0e167b6b10b0bbb984a2
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-12-10 21:13:08 +0900

    메시지 전송 API 구현

--- src/main/java/com/tmax/springkafka/KafkaController.java
+++ src/main/java/com/tmax/springkafka/KafkaController.java
@@ -1,2 +1,21 @@
-package com.tmax.springkafka;public class KafkaController {
+package com.tmax.springkafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+  private final KafkaProducer producer;
+
+  @PostMapping
+  public String sendMessage(@RequestParam("message") String message) {
+    this.producer.sendMessage(message);
+
+    return "success";
+  }
 }
